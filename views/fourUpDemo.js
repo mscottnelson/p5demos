02e0a6abf2ab971c015ed864f3e52f690a0f3e9a
Michael Scott-Nelson <mscottnelson@gmail.com>
2016-10-18 15:55:23 -0400

    create container to demo 4 sketches simultaneously

---
+++ views/fourUpDemo.js
@@ -0,0 +1,28 @@
+function setup() {
+	frameRate(30);
+	colorMode(HSB);
+	var canvas = createCanvas(windowWidth, windowHeight);
+	canvas.parent('sketch-holder');
+	noFill();
+	strokeWeight(5);
+	angleMode(DEGREES);
+}
+
+function draw() {
+	var side = 30;
+	background(0);
+	point(windowWidth/2, windowHeight/2);
+	translate(windowWidth/2, windowHeight/2);
+	for(i = 0; i < 100; i++){
+		stroke(i*4, 100, 100);
+		triangle(-side/2, -side/(2*tan(60)), side/2, -side/(2*tan(60)), 0, sqrt(3)*side/2-side/(2*tan(60)));
+		//rect(0,0,side,side);
+		rotate(180+frameCount/32);
+		side = side + mouseX/8;
+	}
+}
+
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
